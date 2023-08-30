d7f5e076b794ba818d51f73e05e49a261bf66bef
liamjoh <133021562+liamjoh@users.noreply.github.com>
2023-08-30 13:15:29 +0200

    Add files via upload
    This is the text-message function split off from the main script

---
+++ Text-messagefunction.ps1
@@ -0,0 +1,21 @@
+﻿$apiEndpoint = "YOUR_API_ENDPOINT_HERE"
+$apiKey = "YOUR_API_KEY_HERE"
+
+
+
+$jsonPayload = @{
+    "apiKey" = $apiKey
+    "content-type" = "application/json"
+    "to" = $UserMobile
+    "body" = "YOUR_TEXT_MESSAGE_HERE"
+    "smsSender" = "SENDERNAME"
+    "sendNow" = $true
+} | ConvertTo-Json
+
+
+$headers = @{
+    "Content-Type" = "application/json;charset-UTF8"
+    "api-key" = $apiKey
+}
+
+Invoke-RestMethod -Uri $apiEndpoint -Method POST -Headers $headers -Body $jsonPayload
